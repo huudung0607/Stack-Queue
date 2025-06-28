0cdd8fc658c9dbb867ae80e9df8c7250970774e2
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-06-28 18:39:46 +0700

    Create Trung tố - hậu tố.cpp

---
+++ Trung tố - hậu tố.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+#include <climits>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+using namespace std;
+void solve(string s) {
+	stack<string> st;
+	for (int i = s.size() - 1; i >= 0; i--) {
+		if (isalpha(s[i])) {
+			st.push(string(1, s[i]));
+		}
+		else {
+			string a1 = st.top();
+			st.pop();
+			string a2 = st.top();
+			st.pop();
+			string tmp = "(" + a1 + s[i] + a2 + ")";
+			st.push(tmp);
+		}
+	}
+	cout << st.top();
+}
+int main()
+{
+	string s; cin >> s;
+	solve(s);
+}
+
