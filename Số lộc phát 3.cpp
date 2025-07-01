6c92b73cdc8f9a146d27a7ffcc0e8168a4b3adef
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-07-01 07:42:38 +0700

    Create Số lộc phát 3.cpp

---
+++ Số lộc phát 3.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <cmath>
+#include <numeric>
+#include <climits>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+using namespace std;
+vector<long long> v;
+void init() {
+	queue<long long> q;
+	q.push(6);
+	q.push(8);
+	while (!q.empty()) {
+		long long u = q.front();
+		q.pop();
+		if (u > 1e18) break;
+		v.push_back(u);
+		q.push(u * 10 + 6);
+		q.push(u * 10 + 8);
+	}
+}
+int main() {
+	init();
+	int t; cin >> t;
+	sort(v.begin(), v.end(),greater<long long>());
+	while (t--) {
+		int u; cin >> u;
+		vector<long long> res;
+		for (long long x : v) {
+			string s = to_string(x);
+			if (s.size() <= u) {
+				res.push_back(x);
+			}
+		}
+		cout << res.size() << endl;
+		for (auto x : res) {
+			cout << x << " ";
+		}
+		cout << endl;
+	}
+}
